c17429e163d5fbf30da8ae8f8b8025874d4fda05
DhruvVasisht <vasisht.dhruv7@gmail.com>
2023-03-16 16:36:22 +0530

    Java

--- Bird.java
+++ Bird.java
@@ -1,4 +1,27 @@
-class Bird{
+class Features{
+    String name;
+    String color;
+    String origin;
+    int speed;
+    int lifetime;
+    float maxweight;
+    String flying;
+    String eggs;
+    int size;
+
+    void display(){
+        System.out.println("Name Of The Birds:" +name);
+        System.out.println("Name Of The Color:" +color);
+        System.out.println("Name Of The Origin:" +origin);
+        System.out.println("Speed Of The Bird:" +speed);
+        System.out.println("Lifetime Of The Bird:" +lifetime);
+        
+    }
+
+
+}
+
+class Bird {
    public static void main(String args[]){
    System.out.println();
    }

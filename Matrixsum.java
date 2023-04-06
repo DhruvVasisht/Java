3dd9267339afe7b316b446900ba2918b103f91d9
DhruvVasisht <vasisht.dhruv7@gmail.com>
2023-04-06 14:32:45 +0530

    Java

--- Matrixsum.java
+++ Matrixsum.java
@@ -1,47 +1,62 @@
-import java.util.Scanner;  
-public class Matrixsum  
-{  
-public static void main(String[] args)   
-{  
-int m,n;  
-Scanner sc=new Scanner(System.in);  
-System.out.print("Enter the number of rows and cols you want to store: ");  
-
-
-m=sc.nextInt();
-n=sc.nextInt();  
-
-int[][] a = new int[10][10];  
-int[][] b = new int[10][10];
-System.out.println("Enter the elements of the first array: ");  
-for(int i=0;i<m;i++)
-for(int j=0; j<n; j++)  
-{  
+import java.util.Scanner;
+public class Matrixsum
 {
- 
-a[i][j]=sc.nextInt();  
-} 
-} 
-System.out.println("Enter the elements of the second array: ");  
-for(int i=0;i<m;i++)
-for(int j=0; j<n; j++)  
-{  
-{
+    public static void main(String[] args)
+    {
+        int rows,cols;
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of rows and cols you want to store: ");
+        rows=sc.nextInt();
+        cols=sc.nextInt();
+        int[][] a = new int[10][10];
+        int[][] b = new int[10][10];
+        System.out.println("Enter the elements of the first matrix: ");
+        for(int i=0;i<rows;i++)
+            for(int j=0; j<cols; j++)
+            {
+                {
 
-b[i][j]=sc.nextInt();  
-} 
-}
-int c[][]=new int[10][10];  
- System.out.println("Array after addition: ");     
-   
-	for(int i=0;i<m;i++)
-		{    
-		for(int j=0;j<n;j++)
-		    {    
-		  c[i][j]=a[i][j]+b[i][j];   
-		  System.out.print(c[i][j]+" ");    
-		    }    
-		System.out.println();
-		}  
-}  
-}
+                    a[i][j]=sc.nextInt();
+                }
+            }
+        System.out.println(" first matrix is : ");
+        for(int i=0;i<rows;i++){
+            for(int j=0; j<cols; j++)
+
+                {
+
+                    System.out.print(a[i][j]+" ");
+                }
+            System.out.println();
+            }
+        System.out.println("Enter the elements of the second matrix: ");
+        for(int i=0;i<rows;i++)
+            for(int j=0; j<cols; j++)
+            {
+                {
+
+                    b[i][j]=sc.nextInt();
+                }
+            }
+
+
+        System.out.println(" second matrix is : ");
+        for(int i=0;i<rows;i++){
+            for(int j=0; j<cols; j++)
+
+                {
+                    System.out.print(b[i][j]+" ");
+                }
+            System.out.println();
+            }
+        int c[][]=new int[10][10];
+        System.out.println("Array after addition: ");
+        for(int i=0;i<rows;i++)
+        {
+            for(int j=0;j<cols;j++)
+            {
+                c[i][j]=a[i][j]+b[i][j];
+                System.out.print(c[i][j]+" ");
+            }
+            System.out.println();
+		}}}

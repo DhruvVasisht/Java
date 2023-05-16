b13ea492a1d0323a02e30dc37d26ddc762825e5d
DhruvVasisht <vasisht.dhruv7@gmail.com>
2023-05-16 23:28:15 +0530

    Java

--- demo.java
+++
@@ -1,8 +0,0 @@
-class demo {
-    public static void main(String a[]) {
-        System.out.println("Hello Java ");
-        System.out.print("How are U");
-        
-    }
-    
-}
---
+++ student.java
@@ -0,0 +1,17 @@
+public class student {
+    int id;
+    String name;
+}
+class TestStudent2{
+    public static void main(String args[]){
+        student s1=new student();
+        s1.id=102;
+        s1.name="Dhruv Vashisht";
+        System.out.println("student_id- "+s1.id);
+        System.out.println("studnt_name- "+s1.name);
+    }
+}
+
+
+
+

2fc1f2831e61056a21d57ffb3d9076e578fe7163
DhruvVasisht <vasisht.dhruv7@gmail.com>
2023-05-16 23:23:53 +0530

    Java

---
+++ DataTypes.java
@@ -0,0 +1,18 @@
+class data_types {
+	public static void main(String args[]){
+	
+		char a = 'D';
+		int i = 2;
+		byte b = 1;
+        short s = 96;
+		double d = 5.355453532;
+		float f = 21.7333434f;
+		long l = 921234;
+        System.out.println("char: " + a);
+		System.out.println("integer: " + i);
+		System.out.println("byte: " + b);
+		System.out.println("short: " + s);
+		System.out.println("float: " + f);
+		System.out.println("double: " + d);
+		System.out.println("long: " + l);
+	}}
--- Type.java
+++
@@ -1,7 +0,0 @@
-class type{
-   public static void main(String args[]){
-    double mydouble= 3.14;
-    int mdouble=(int)mydouble;
-   System.out.println(mdouble);
-   }
-}
